c43dde58ced369093c20f034b3e03e66c5200c49
mullen25312 <mail@benjaminjahn.de>
2022-12-01 07:34:11 +0100

    feat: Solve d01

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -5,9 +5,32 @@
 fn main() {
     println!("########### d01 ###########");
 
-    let file = String::from("./demo.txt");
-    // let file = String::from("./input.txt");
+    // let file = String::from("./demo.txt");
+    let file = String::from("./input.txt");
 
     let contents = std::fs::read_to_string(file).expect("Should have been able to read the file");
-    // let data: Vec<i32> = contents.lines().map(str::parse).map(Result::unwrap).collect();
+    let lines: Vec<&str> = contents.lines().collect();
+
+    // parse input
+    let mut elves: Vec<Vec<i32>> = Vec::new();
+    let mut tmp: Vec<i32> = Vec::new();
+    for line in lines {
+        if line != "" {
+            tmp.push(line.parse().unwrap_or(0))
+        } else {
+            elves.push(tmp.clone());
+            tmp.clear();
+        }
+    }
+    elves.push(tmp.clone());
+
+    elves.sort_by_key(|elf| -elf.iter().sum::<i32>());
+
+    // part one
+    let result1: i32 = elves[0].iter().sum::<i32>();
+    println!("Result of part one: {}", result1);
+
+    // part two
+    let result2: i32 = elves[0].iter().sum::<i32>() + elves[1].iter().sum::<i32>() + elves[2].iter().sum::<i32>();
+    println!("Result of part one: {}", result2);
 }
